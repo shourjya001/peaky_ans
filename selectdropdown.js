4ff3132e56e99791fac8bbe4ab4f0e6bdfe27c9f
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2024-10-22 00:19:52 +0530

    Update selectdropdown.js

--- selectdropdown.js
+++ selectdropdown.js
@@ -1,5 +1,5 @@
 function selectdropdown(s1, s2) {
-    var s3 = (s2 == 'code') ? 'name' : 'code';
+    var s3 = (s2 === 'code') ? 'name' : 'code';
 
     // Show loading
     var loadingWrap = document.all["loading_wrap"];
@@ -20,9 +20,9 @@ function selectdropdown(s1, s2) {
     var grpid = selectedValue;
 
     // Perform actions based on s1
-    if (s1 == 'sgr') {
+    if (s1 === 'sgr') {
         commonsearch('fetchLEBasedonSGR', grpid, searchParam, 'le', 0);
-    } else if (s1 == 'le') {
+    } else if (s1 === 'le') {
         inputfieldsearch("select" + s1 + "_" + s3); // updating other dropdown
         commonsearch('fetchLegalEntityBasedonID', grpid, searchParam, 'le', 1);
 
