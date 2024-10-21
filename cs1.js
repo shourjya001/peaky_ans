ad8cfb7dd9fdf647ce8753e78e5e2303112180f6
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2024-10-21 22:13:41 +0530

    Create cs1.js

---
+++ cs1.js
@@ -0,0 +1,54 @@
+function commonsearch(searchType, searchString, searchParam, stype, check) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "dbe_cfl_user_accessTransferSave.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log("AJAX request successful");
+            var resultData = JSON.parse(xhr.responseText);
+            if (resultData) {
+                console.log("Result data received:", resultData);
+                if (check === 0) populateSearchOptions(stype, resultData);
+                if (stype === 'sgr') {
+                    commonsearch('fetchLEBasedonSGR', resultData[0]['id'], searchParam, 'le', 0);
+                } else if (stype === 'le') {
+                    if (document.getElementById("selectsgr_code").value !== '' && document.getElementById("selectle_code").value !== '') {
+                        checkForCreditFiles(document.getElementById("selectsgr_code").value, document.getElementById("selectle_code").value);
+                    } else if (document.getElementById("txtle_code").value !== '' || document.getElementById("txtle_name").value !== '') {
+                        var sgrdata = { 'sgr': { 'id': resultData[0].sgr_code, 'name': resultData[0].sgr_name } };
+                        populateSearchOptions('sgr', sgrdata);
+                        var ledata = { 'le': { 'id': resultData[0].le_code, 'name': resultData[0].le_name } };
+                        populateSearchOptions('le', ledata);
+                        checkForCreditFiles(document.getElementById("selectsgr_code").value, document.getElementById("selectle_code").value);
+                    } else {
+                        if (check === 0) {
+                            populateSearchOptions('sgr', resultData);
+                            commonsearch('fetchLegalEntityBasedonID', resultData[0]['id'], searchParam, 'le', 1);
+                        }
+                    }
+                }
+                document.getElementById("loading_wrap").style.display = 'none';
+            } else {
+                if (check === 0) {
+                    alert("No Matching Results Found...! or You are not authorized to see this SGR/LE, please contact the Banking Administrator.");
+                }
+                document.getElementById("loading_wrap").style.display = 'none';
+                if (stype === 'le') {
+                    if (check === 1) {
+                        document.getElementById("codria_span").innerHTML = '';
+                        alert("No references are available for this LE.");
+                    } else {
+                        emptydropdown(stype);
+                    }
+                } else if (stype === 'sgr') {
+                    var rtype = 'le';
+                    emptydropdown(stype);
+                    emptydropdown(rtype);
+                }
+            }
+        }
+    };
+
+    xhr.send("searchType=" + encodeURIComponent(searchType) + "&searchString=" + encodeURIComponent(searchString));
+}
