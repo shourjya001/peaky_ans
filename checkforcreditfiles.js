bcd18c60baf153d5fa041c94f50532d3d6736ec1
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2024-10-22 21:52:01 +0530

    Update checkforcreditfiles.js

--- checkforcreditfiles.js
+++ checkforcreditfiles.js
@@ -10,34 +10,41 @@ function checkForCreditFiles(codspm, codle) {
     }
 
     xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            if (response.id === 'None') {
-                document.querySelectorAll(".codria_class").forEach(function(element) {
-                    element.style.display = 'none';
-                });
-                alert("No credit files available..!");
-            } else if (response.id !== 'None') {
-                response.forEach(function(item) {
-                    var input = document.createElement('input');
-                    input.type = 'radio';
-                    input.name = 'codria';
-                    input.style.display = 'inline';
-                    input.style.paddingRight = '5px';
-                    input.style.width = '20%';
-                    input.id = 'codria' + item.codri;
-                    input.value = item.codria;
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                if (response.id === 'None') {
+                    var elements = document.getElementsByClassName("codria_class");
+                    for (var i = 0; i < elements.length; i++) {
+                        elements[i].style.display = 'none';
+                    }
+                    alert("No credit files available..!");
+                } else {
+                    for (var i = 0; i < response.length; i++) {
+                        var item = response[i];
+                        var input = document.createElement('input');
+                        input.type = 'radio';
+                        input.name = 'codria';
+                        input.style.display = 'inline';
+                        input.style.paddingRight = '5px';
+                        input.style.width = '20%';
+                        input.id = 'codria' + item.codri;
+                        input.value = item.codria;
 
-                    var label = document.createElement('label');
-                    label.htmlFor = input.id;
-                    label.innerText = item.codria;
+                        var label = document.createElement('label');
+                        label.htmlFor = input.id;
+                        label.innerText = item.codria;
 
-                    document.getElementById('codria_span').appendChild(input);
-                    document.getElementById('codria_span').appendChild(label);
-                });
-                document.querySelectorAll(".codria_class").forEach(function(element) {
-                    element.style.display = 'block';
-                });
+                        document.getElementById('codria_span').appendChild(input);
+                        document.getElementById('codria_span').appendChild(label);
+                    }
+                    var elements = document.getElementsByClassName("codria_class");
+                    for (var i = 0; i < elements.length; i++) {
+                        elements[i].style.display = 'block';
+                    }
+                }
+            } else {
+                alert("AJAX request failed with status: " + xhr.status);
             }
         }
     };
@@ -46,9 +53,4 @@ function checkForCreditFiles(codspm, codle) {
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send();
-}
-
-function goToHomePage() {
-    alert("te");
-    location.reload(true);
 }
