179317ba82cb4ea76cbfc2b1a3f5437cc90ca12d
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2024-10-25 01:53:15 +0530

    Update checkforcreditfiles.js

--- checkforcreditfiles.js
+++ checkforcreditfiles.js
@@ -13,42 +13,48 @@ function checkForCreditFiles(codspm, codle) {
             if (xhr.status === 200 || window.location.href.indexOf("http") === -1) {
                 alert("AJAX request successful");
                 try {
-                    var response = JSON.parse(xhr.responseText);
-                    if (response.id === 'None') {
-                        var elements = document.getElementsByClassName("codria_class");
-                        for (var i = 0; i < elements.length; i++) {
-                            elements[i].style.display = 'none';
-                        }
-                        alert("No credit files available..!");
-                    } else {
-                        var codriaSpan = document.getElementById('codria_span');
-                        if (codriaSpan) {
-                            for (var i = 0; i < response.length; i++) {
-                                var item = response[i];
-                                var input = document.createElement('input');
-                                input.type = 'radio';
-                                input.name = 'codria';
-                                input.style.display = 'inline';
-                                input.style.paddingRight = '5px';
-                                input.style.width = '20%';
-                                input.id = 'codria' + item.codri;
-                                input.value = item.codria;
-
-                                var label = document.createElement('label');
-                                label.htmlFor = input.id;
-                                label.innerText = item.codria;
-
-                                codriaSpan.appendChild(input);
-                                codriaSpan.appendChild(label);
-                            }
+                    var responseText = xhr.responseText;
+                    if (responseText) {
+                        var response = JSON.parse(responseText);
+                        if (response.id === 'None') {
                             var elements = document.getElementsByClassName("codria_class");
                             for (var i = 0; i < elements.length; i++) {
-                                elements[i].style.display = 'block';
+                                elements[i].style.display = 'none';
                             }
+                            alert("No credit files available..!");
                         } else {
-                            console.error("Element with id 'codria_span' not found.");
-                            alert("Element with id 'codria_span' not found.");
+                            var codriaSpan = document.getElementById('codria_span');
+                            if (codriaSpan) {
+                                for (var i = 0; i < response.length; i++) {
+                                    var item = response[i];
+                                    var input = document.createElement('input');
+                                    input.type = 'radio';
+                                    input.name = 'codria';
+                                    input.style.display = 'inline';
+                                    input.style.paddingRight = '5px';
+                                    input.style.width = '20%';
+                                    input.id = 'codria' + item.codri;
+                                    input.value = item.codria;
+
+                                    var label = document.createElement('label');
+                                    label.htmlFor = input.id;
+                                    label.innerText = item.codria;
+
+                                    codriaSpan.appendChild(input);
+                                    codriaSpan.appendChild(label);
+                                }
+                                var elements = document.getElementsByClassName("codria_class");
+                                for (var i = 0; i < elements.length; i++) {
+                                    elements[i].style.display = 'block';
+                                }
+                            } else {
+                                console.error("Element with id 'codria_span' not found.");
+                                alert("Element with id 'codria_span' not found.");
+                            }
                         }
+                    } else {
+                        console.error("Empty response from server.");
+                        alert("Empty response from server.");
                     }
                 } catch (e) {
                     console.error("Error parsing JSON response: " + e.message);
